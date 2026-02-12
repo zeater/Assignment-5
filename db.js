153d7189c50c0dcb3a761c255b29fb4d580880c8
zeater <safweta564@gmail.com>
2026-02-12 21:57:58 +0200

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "112233",
+  password: "1111111",
   database: "store_db"
 });
 
